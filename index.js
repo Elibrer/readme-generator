0e3963f3f5a6f70c3c79db8c218df6235682c151
Elibrer <eli.brer@gmail.com>
2023-04-13 19:21:34 +1000

    Updated js to include addition of license files. Markdown generator function works correctly.

--- index.js
+++ index.js
@@ -3,188 +3,206 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 const generateHTML = require('./utils/generateHTML.js');
+const generateLicense = require('./utils/generateLicense.js');
 
+const dayjs = require('dayjs')
 inquirer.registerPrompt('recursive', require('inquirer-recursive'));
 
-
-
+function renderYear() {
+    const currentYear = dayjs().format('YYYY'); 
+    console.log(currentYear);
+    return currentYear;  
+}
 
 // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
 // TODO: Create an array of questions for user input
+const fileTypeQuestion = [
+    {
+        type: 'list',
+        name: 'fileType',
+        message: 'What file would you like to create?',
+        choices: ['README', 'Blank HTML', 'Other'],
+    },
+];
+
 const questions = [
     {
-    type: 'list',
-    name: 'fileType',
-    message: 'What file would you like to create?',
-    choices: ['README', 'Blank HTML', 'Other'],
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
     },
-    // {
-    // type: 'input',
-    // name: 'title',
-    // message: 'What is the title of your project?',
-    // },
-    // {
-    // type: 'input',
-    // name: 'titleDescription',
-    // message: 'Please add a short title description of your project:',
-    // },
-    // {
-    // type: 'checkbox',
-    // name: 'languages',
-    // message: 'Which languages did you use in your project?',
-    // choices: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Node.js', 'Express.js', 'MySQL', 'MongoDB', 'React.js', 'Bootstrap', 'Materialize', 'Handlebars.js', 'Other'],
-    // },
-    // {
-    // type: 'input',
-    // name: 'repoLink',
-    // message: 'Please provide a link to your project repository:',
-    // },
-    // {
-    // type: 'input',
-    // name: 'deployedLink',
-    // message: 'Please provide a link to your deployed project:',
-    // },
     {
     type: 'input',
-    name: 'numberPets',
-    message: 'Number of pets',
-    validate: function(answers) {
-        console.log(answers + "what\n")
-        if (answers > 0) {
-            console.log("hello")
-            return repeatQuestion()
-        }else{
-            console.log("end")
-            return
-        }
-    }
+    name: 'titleDescription',
+    message: 'Please add a short title description of your project:',
     },
-    // {
-    // type: 'recursive',
-    // name: 'contributors',
-    // message: 'Enter the names and links of the project contributors and their GitHub page:',
-    // shouldPrompt: function(answers) {
-    //   return answers.contributors && answers.contributors.length < 5;
-    // },
-    // prompts: [
-    //   {
-    //     type: 'input',
-    //     name: 'name',
-    //     message: 'Enter the contributor name:'
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'link',
-    //     message: 'Enter the contributor GitHub page link:'
-    //     },
-    //     {
-    //     type: 'confirm',
-    //     name: 'askAgain',
-    //     message: 'Do you want to add another contributor? (just hit enter for YES)',
-    //     default: true
-    //   }
-    // ]
-    // },
-    // {
-    // type: 'input',
-    // name: 'description',
-    // message: 'Please provide a description for your project:',
-    // },
-    // {
-    // type: 'input',
-    // name: 'installation',
-    // message: 'Please provide installation instructions for your project:',
-    // },
-    // {
-    // type: 'recursive',
-    // name: 'features',
-    // message: 'Please provide a list of features for your project:',
-    // prompts: [
-    //     {
-    //     type: 'input',
-    //     name: 'feature',
-    //     message: 'Enter the feature:',
-    //     }
-    // ]
-    // },
-    // {
-    // type: 'input',
-    // name: 'documentation',
-    // message: 'Please provide links to all relevant documentation:',
-    // },
-    // {
-    // type: 'input',
-    // name: 'preview',
-    // message: 'Please provide a link to a preview image of your project:',
-    // },
-    // {
-    // type: 'input',
-    // name: 'previewDescription',
-    // message: 'Please provide a description of the preview image:',
-    // },
-    // {
-    // type: 'input',
-    // name: 'license',
-    // message: 'Please choose the license which this project is covered under:',
-    // },
-    // {
-    // type: 'input',
-    // name: 'tests',
-    // message: 'Please provide testing instructions for your project:',
-    // },
-    // {
-    // type: 'input',
-    // name: 'email',
-    // message: 'Please provide your email address so you may be contacted for any questions and queries:',
-    // },
+    {
+    type: 'checkbox',
+    name: 'languages',
+    message: 'Which languages did you use in your project?',
+    choices: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Node.js', 'Express.js', 'MySQL', 'React.js', 'Bootstrap', 'Tailwind CSS', 'Other'],
+    },
+    {
+    type: 'input',
+    name: 'repoLink',
+    message: 'Please provide a link to your project repository: https://',
+    },
+    {
+    type: 'input',
+    name: 'deployedLink',
+    message: 'Please provide a link to your deployed project: https://',
+    },
+    {
+    type: 'recursive',
+    name: 'contributors',
+    message: 'Add a contributor/s?',
+    prompts: [
+        {
+        type: 'input',
+        name: 'contributorName',
+        message: 'Enter the contributor name:'
+        },
+        {
+        type: 'input',
+        name: 'contributorGitHub',
+        message: 'Enter the contributor GitHub page link: https://'
+        },
+    ]
+    },
 ];
-    
-// TODO: Create a function to write README file
-// function writeToReadmeFile(type,fileName, data) {
-//     fs.writeFile(fileName, generateMarkdown(data), (err) =>
-//     err ? console.error(err) : console.log("Success!")
-//     );
-// }
+
+const questions2 = [
+    {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description for your project:',
+    },
+    {
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide installation instructions for your project:',
+    },
+    {
+    type: 'recursive',
+    name: 'features',
+    message: 'Add feature to the features list?',
+    prompts: [
+        {
+        type: 'input',
+        name: 'feature',
+        message: 'Enter the feature:',
+        }
+    ]
+    },
+];
+
+const questions3 = [
+    {
+        type: 'input',
+        name: 'documentation',
+        message: 'Please provide links to all relevant documentation:',
+    },
+    {
+        type: 'input',
+        name: 'preview',
+        message: 'Please provide a link to a preview image of your project (use https:// for external links):',
+    },
+    {
+        type: 'input',
+        name: 'previewDescription',
+        message: 'Please provide a description of the preview image:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose the license which this project is covered under:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'ISC', 'Other'],
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide testing instructions for your project:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email address so you may be contacted for any questions and queries:',
+    },
+];
 
 function writeToFile(type, fileName, data) {
-    fs.writeFile(fileName, eval(`generate${type}()`), (data), (err) =>
+    fs.writeFile(eval(`'./generated-files/${fileName}'`), eval(`generate${type}`)(data), (data), (err) =>
     err ? console.error(err) : console.log("Success!")
     );
 }
 
-function repeatQuestion() {
-    return inquirer.prompt([
-        {
-        type: 'input',
-        name: 'repeated',
-        message: 'blabla'
+function createLicenseFile(data) {
+    const year = renderYear();
+    let contributorNames = '';
+    for (let i = 0; i < data.contributors.length; i++) {
+        if (i === data.contributors.length - 1) {
+        contributorNames += `${data.contributors[i].contributorName}`;
         }
-    ])
+        else {
+        contributorNames += `${data.contributors[i].contributorName}, `;
+        }   
+    }
+    
+    if (data.license !== ``) {
+        fs.writeFile(`./generated-files/LICENSE.md`, generateLicense(data, contributorNames, year), (data), (err) =>
+        err ? console.error(err) : console.log("Success!")
+        );
+    }
 }
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then((data) => {
-        let fileName = ``;
-        let type = ``;
+// License files
+// File location
 
-        switch (data.fileType) {
+
+
+
+//The reason for multiple question objects is that 
+// for some reason the 'recursive' prompt prematurely ends 
+// the function if there is another prompt straight after it 
+// and the user selects 'no'. Having the recursive prompts
+// in separate objects seems to fix this issue - but I still
+// needed to merge the seperate objects into one object.
+function init() {
+    let fileName = ``;
+    let type = ``;
+    let dataObj = {}
+    inquirer.prompt(fileTypeQuestion)
+    .then((typeData) => {
+        switch (typeData.fileType) {
             case 'Blank HTML':
                 fileName = `index.html`;
                 type = 'HTML';
-                writeToFile(type, fileName, data);
+                writeToFile(type, fileName);
                 break;
             case 'README':
                 type = 'Markdown';
                 fileName = `README.md`;
-                writeToFile(type, fileName, data);
+                inquirer.prompt(questions)
+                .then((data) => { 
+                    inquirer.prompt(questions2)
+                    .then((data2) => {
+                        inquirer.prompt(questions3)
+                        .then((data3) => {
+                            dataObj = {
+                                ...data,
+                                ...data2,
+                                ...data3,
+                            }
+                            createLicenseFile(dataObj)
+                            writeToFile(type, fileName, dataObj);
+                        })
+                    })
+                })
                 break;
             default:
                 break;
         }
-
     })
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,69 +1,166 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'GNU GPLv3':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'Apache 2.0':
+      return '![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-green.svg)';
+    case 'BSD 3-Clause':
+      return '![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+    case 'ISC':
+      return '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)'; 
+    case 'None':
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    switch (license) {
+        case 'MIT':
+            return '[Licensed under the MIT license.](https://opensource.org/licenses/MIT)';
+        case 'GNU GPLv3':
+            return '[Licensed under the GNU GPLv3 license.](https://www.gnu.org/licenses/gpl-3.0)';
+        case 'Apache 2.0':
+            return '[Licensed under the Apache 2.0 license.](https://opensource.org/licenses/Apache-2.0)';
+        case 'BSD 3-Clause':
+            return '[Licensed under the BSD 3-Clause license.](https://opensource.org/licenses/BSD-3-Clause)';
+        case 'ISC':
+            return '[Licensed under the ISC license.](https://opensource.org/licenses/ISC)';
+        case 'None':
+            return '';
+    }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLanguagesBadge(language) {
+  switch (language) {
+    case 'HTML':
+      return '![HTML](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white)';
+    case 'CSS':
+      return '![CSS](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white)';
+    case 'JavaScript':
+      return '![JavaScript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)';
+    case 'jQuery':
+      return '![jQuery](https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white)';
+    case 'Node.js':
+      return '![Node.js](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)';
+    case 'Express.js':
+      return '![Express.js](https://img.shields.io/badge/Express.js-404D59?style=for-the-badge)';
+    case 'MySQL':
+      return '![MySQL](https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white)';
+    case 'React.js':
+      return '![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)';
+    case 'Bootstrap':
+      return '![Bootstrap](https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white)';
+    case 'Tailwind CSS':
+      return '![Tailwind CSS](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)';
+    case 'Other':
+      return '![Other](https://img.shields.io/badge/Other-000000?style=for-the-badge&logo=github&logoColor=white)';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  const {title, titleDescription, languages, repoLink, deployedLink, contributors, description, installation, features, documentation, preview, previewDescription, license, tests, email} = data;
+function generateMarkdown(dataObj) {
+  let {title, titleDescription, languages, repoLink, deployedLink, contributors, description, installation, features, documentation, preview, previewDescription, license, tests, email} = dataObj;
   uppercaseTitle = title.charAt(0).toUpperCase() + title.slice(1);
-  languagesSpaced = languages.join(', ');
+  
+  let languageBadges = '';
+  for (let i=0; i<languages.length; i++){
+    languageBadges += `${renderLanguagesBadge(languages[i])} `;
+  }
 
-  return `
-  ## ${uppercaseTitle}
-  *${titleDescription}*
+  let licenseBadge = renderLicenseBadge(license);
 
-  ## Languages used
-  ${languagesSpaced}
+  let contributorsString = '';
+  for(let i=0; i<contributors.length; i++){
+    uppercaseName = contributors[i].contributorName.charAt(0).toUpperCase() + contributors[i].contributorName.slice(1);
+    contributorsString += `> ${uppercaseName} @[GitHub](https://${contributors[i].contributorGitHub})\n > \n`;
+  }
+  
+  let featuresString = '';
+  for(let i=0; i<features.length; i++){
+    featuresString += `- ${features[i].feature} \n`;
+  }
 
-  ## Links
-  - [GitHub Repository](${repoLink})
-  - [Deployed Application](${deployedLink})
+  if (documentation === '') {
+    documentation = 'No documentation provided.';
+  } else (
+    documentation = `[${documentation}](${documentation})`
+  )
 
-  ## Contributors
-  - ${contributors}
+  let imgPreview ='';
+  if (preview === '') {
+    imgPreview = 'No preview provided.';
+  } else {
+    imgPreview = `![${previewDescription}](${preview})`
+  }
 
-  ## Table of Contents
-  - [Description](#description)
-  - [Installation](#installation)
-  - [Features](#features)
-  - [Documentation](#documentation)
-  - [Application Preview](#full-preview)
-  - [License](#license)
-  - [Tests](#tests)
-  - [Questions](#questions)
+  let licenseLink = '';
+  if (license === 'None') {
+    licenseLink = 'No license provided.';
+  } else {
+    licenseLink = renderLicenseLink(license);
+  }
 
-  ## Description
-  ${description}
 
-  ## Installation
-  ${installation}
+  //The template literal data that will be entered into the new README file
+  return `## ${uppercaseTitle}
+${licenseBadge}
 
-  ## Features
-  - ${features}
+---
 
-  ## Documentation
-  ${documentation}
+*${titleDescription}*
 
-  ## Application Preview
-  ![${previewDescription}](${preview})
+## Languages used
+${languageBadges}
 
-  ## License
-  ${license}
+## Links
+>
+> - [GitHub Repository](https://${repoLink})
+> 
+> - [Deployed Application](https://${deployedLink})
 
-  ## Tests
-  ${tests}
+## Contributors
+${contributorsString}
 
-  ## Questions
-  For questions and enquiries, please contact me at: 
-  [${email}](${email})
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Features](#features)
+- [Documentation](#documentation)
+- [Application Preview](#full-preview)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${description}
+
+## Installation
+${installation}
+
+## Features
+${featuresString}
+
+## Documentation
+${documentation}
+
+## Application Preview
+${imgPreview}
+
+## License
+${licenseLink}
+
+## Tests
+${tests}
+
+## Questions
+For questions and enquiries, please contact me at: 
+[${email}](${email})
 `;
 }
 
